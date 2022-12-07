755aeba2dc3a79166ec08a1a65d9da3bcaa9eafc
srwoo <rhak12@naver.com>
2022-12-07 19:06:03 +0900

    96-99 api

--- client/src/components/Floating_population/floatingPopulationList.js
+++ client/src/components/Floating_population/floatingPopulationList.js
@@ -19,8 +19,8 @@ class floatingPopulationList extends Component {
         axios.get('https://t-data.seoul.go.kr/apig/apiman-gateway/tapi/v2xBusStationCrowdedInformation/1.0?apiKey=c6aa2791-eb11-46be-b44a-bea3cb9d180a&type=json&pageNo=1&numOfRows=10')
         .then((response) => {
             try {
-                this.setState({ responseFPList: response });
-                this.setState({ append_FPList: this.FloatPopulListAppend() });
+                //this.setState({ responseFPList: response });
+                this.setState({ append_FPList: this.FloatPopulListAppend(response) });
             } catch (error) {
                 alert(error)
             }
@@ -28,26 +28,31 @@ class floatingPopulationList extends Component {
         .catch( error => {alert(error);return false;} );
     }
 
-    FloatPopulListAppend = () => {
-        var this_bind = this;
+    FloatPopulListAppend = (response) => {
         let result = []
-        var json = this_bind.state.responseFPList.data
+        //var json = this.state.responseFPList.data
+        var json = response.data;
         //var jsonString = JSON.stringify(FPList)
         //var json = JSON.parse(jsonString)
 
         for(let i=0; i<json.length; i++){
             var data = json[i]
             var idx = i+1
+
             result.push(
-                <ul className="hidden_type">
-                    <li>{idx}</li>
-                    <li>{data.cameraId}</li>
-                    <li>{data.itisCd}</li>
-                    <li>{data.trsmUtcTime}</li>
-                    <li>{data.trsmYear}</li>
-                    <li>{data.trsmTm}</li>
-                    <li>{data.trsmMs}</li>
-                </ul>
+                <table key={idx} className="table_ty2 fp_tlist">
+                    <tbody>
+                        <tr className="hidden_type">
+                            <td>{idx}</td>
+                            <td>{data.cameraId}</td>
+                            <td>{data.itisCd}</td>
+                            <td>{data.trsmUtcTime}</td>
+                            <td>{data.trsmYear}</td>
+                            <td>{data.trsmTm}</td>
+                            <td>{data.trsmMs}</td>
+                        </tr>
+                    </tbody>
+                </table>
             )
         }
         return result
@@ -61,15 +66,19 @@ class floatingPopulationList extends Component {
                         <h2 className="s_tit1">버스 승강장 혼잡 정보 서비스</h2>
                     </div>
                     <div className="list_cont list_cont_admin">
-                        <ul className="table_ty1 fp_tlist">
-                            <li>Row</li>
-                            <li>카메라ID</li>
-                            <li>ITIS코드</li>
-                            <li>전송UTC시간</li>
-                            <li>패킷전송일자</li>
-                            <li>패킷전송시간</li>
-                            <li>패킷전송밀리초</li>
-                        </ul>
+                        <table className="table_ty1 fp_tlist">
+                            <thead>
+                                <tr>
+                                    <th>Row</th>
+                                    <th>카메라ID</th>
+                                    <th>ITIS코드</th>
+                                    <th>전송UTC시간</th>
+                                    <th>패킷전송일자</th>
+                                    <th>패킷전송시간</th>
+                                    <th>패킷전송밀리초</th>
+                                </tr>
+                            </thead>
+                        </table>
                         {this.state.append_FPList}
                     </div>
                 </article>
