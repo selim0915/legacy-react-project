d7e36cec70d0b4e87f8264fe1bc2060274bd9a1c
srwoo <rhak12@naver.com>
2022-12-13 18:14:45 +0900

    react-cookie, logout

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { Route } from "react-router-dom";
-// import {connect} from 'react-redux';
+import cookie from 'react-cookies';
+import axios from "axios";
+
 import SoftwareList from './SoftwareToolsManage/SoftwareList';
 import SoftwareView from './SoftwareToolsManage/SoftwareView';
 import floatingPopulationList from './Floating_population/floatingPopulationList';
-//import '../App.css';
-//import 'bootstrap/dist/css/bootstrap.css';
 import Register from './Register/Register';
 
 // css
+//import '../App.css';
+//import 'bootstrap/dist/css/bootstrap.css';
 import '../css/new.css';
 
 // header
@@ -21,37 +23,71 @@ import Footer from './Footer/Footer';
 import LoginForm from './LoginForm';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  componentDidMount() {
+    axios.post('/api/LoginForm?type=SessionConfirm', {
+      token1 : cookie.load('userid'),
+      token2 : cookie.load('username') 
+    })
+    .then( response => {
+        this.state.userid = response.data.token1
+        let password = cookie.load('userpassword')
+
+        if(password !== undefined){
+          axios.post('/api/LoginForm?type=SessionSignin', {
+            is_Email: this.state.userid,
+            is_Token : password
+          })
+          .then( response => {
+            if(response.data.json[0].useremail === undefined){
+              this.noPermission()
+            }
+          })
+          .catch( error => {
+            this.noPermission()
+          });
+        }else{
+          this.noPermission()
+        }
+    })
+    .catch( response => this.noPermission());
+  }
+
+  noPermission = (e) => {
+    if(window.location.hash != 'nocookie'){
+      this.remove_cookie();
+
+      setTimeout(function() {
+          window.location.href = '/login/#nocookie';
+      }.bind(this),1000);
+    }
+  };
+
+  remove_cookie = (e) => {
+    cookie.remove('userid', { path: '/'});
+    cookie.remove('username', { path: '/'});
+    cookie.remove('userpassword', { path: '/'});
+  }
+
   render() {
     return (
       <div className="App">
         <HeaderAdmin />
         <Route exact path='/' component={LoginForm} />
+        <Route path='/login' component={LoginForm} />
         <Route path='/SoftwareList' component={SoftwareList} />
         <Route path='/SoftwareView/:swtcode' component={SoftwareView} />
         <Route path='/floatingPopulationList' component={floatingPopulationList} />
         <Route path='/register' component={Register} />
         <Footer />
       </div>
-      // <div>
-      //   <h1>react 시작</h1>
-      //   <p>html 적용</p>
-      //   {/* <span>{this.props.store.getState().data.str}</span><br/>
-      //   <StrAddButton store={this.props.store}/> */}
-
-      //   {/* <span>{this.props.str}</span><br/>
-      //   <StrAddButton AppProp="200"/> */}
-      // </div>
     );
   }
 }
-
-// let mapStateToProps = (state, props) => {
-//   console.log("props from index.js : "+props.indexProp)
-//   return {
-//     str : state.data.str,
-//   }
-// }
-
-// App = connect(mapStateToProps, null)(App);
 
 export default App;
