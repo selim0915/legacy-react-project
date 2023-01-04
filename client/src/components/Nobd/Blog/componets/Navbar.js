93208e6612927afa12f0503c1b0736297f678655
srwoo <rhak12@naver.com>
2023-01-05 00:42:33 +0900

    Add Router Page

--- client/src/components/Nobd/Blog/componets/Navbar.js
+++ client/src/components/Nobd/Blog/componets/Navbar.js
@@ -1,13 +1,19 @@
 
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return(
         <nav className="navbar navbar-light" style={{'backgroundColor':'#e3f2fd', 'padding':'10px 20px'}}>
             <Link className="navbar-brand" to="/">Home</Link>
             <ul className="navbar-nav">
-                <li className="nav-item active">
-                    <Link className="nav-link" to="/Blog">Blog</Link>
+                <li className="nav-item">
+                    <NavLink
+                        exact
+                        activeClassName="active"
+                        className="nav-link"
+                        aria-current ="page"
+                        to="/Blog">Blog
+                    </NavLink>
                 </li>
             </ul>
         </nav>
--- client/src/js/snow.js
+++ client/src/js/snow.js
@@ -8,7 +8,7 @@ function makeSnowflake() {
     const duration = Math.random() * 20 + MIN_DURATION;
 
     snowflake.classList.add("snowflake");
-    snowflake.style.left = `${Math.random() * window.screen.width}px`;
+    snowflake.style.left = `${Math.random() * window.screen.width-200}px`;
     snowflake.style.animationDelay = `${delay}s`;
     snowflake.style.opacity = initialOpacity;
     snowflake.style.animation = `fall ${duration}s linear`;
