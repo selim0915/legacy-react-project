7544d66f8cefb41dfcbc1709bc5dac4a290b2a3f
srwoo <rhak12@naver.com>
2023-01-04 15:54:17 +0900

    basic react

--- client/src/components/Nobd/Blog/Blog.js
+++ client/src/components/Nobd/Blog/Blog.js
@@ -1,13 +1,25 @@
-import React, { Component } from "react";
+import React, { Component, Fragment, useState } from "react";
 
-class Blog extends Component {
-    render() {
-        return (
-            <div>
-                blog
-            </div>
-        )
-    }
+function Blog() {
+    const [number, setNumber] = useState(1);
+
+    const double = () => {
+        //const doubleNumber = number * 2;
+
+        // 이전 값을 활용하여 값을 세팅하는 방법
+        setNumber((number) => number * 2); // 1줄작성
+        setNumber((number) => { // 2줄작성
+            return number * 2
+        });
+    };
+
+    return (
+        // <Fragment></Fragment>와 <></>는 같다
+        <>
+            <div>{number}</div>
+            <button className="btn btn-primary" onClick={double}>submit</button>
+        </>
+    )
 }
 
 export default Blog;
