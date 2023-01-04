18b03afafde62a7a0b6d7aae6d61255b2d237988
srwoo <rhak12@naver.com>
2023-01-05 01:08:48 +0900

    Add Bolg list(useEffect)

--- client/src/components/Nobd/Blog/pages/ListPage.js
+++ client/src/components/Nobd/Blog/pages/ListPage.js
@@ -1,4 +1,19 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 const ListPage = () => {
+    const [posts, setPosts] = useState([]);
+
+    const getPosts = () => {
+        axios.get('http://localhost:3001/posts').then((res) => {
+            setPosts(res.data);
+        })
+    }
+
+    useEffect(() => {
+        getPosts();
+    }, []); // 처음실행할때, 삭제 될때만 사용되는 함수
+
     return(
         <>
             <h2>
