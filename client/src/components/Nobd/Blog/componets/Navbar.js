9714f384a32f90f104b9e16c5abf62234a873efb
srwoo <rhak12@naver.com>
2023-01-10 01:44:08 +0900

    Add 간단한 auth slic

--- client/src/components/Nobd/Blog/componets/Navbar.js
+++ client/src/components/Nobd/Blog/componets/Navbar.js
@@ -1,11 +1,26 @@
 
+import { useDispatch, useSelector } from "react-redux";
 import { Link, NavLink } from "react-router-dom";
+import { login, logout} from "../../../../store/authSlice";
 
 const Navbar = () => {
+    const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+    const dispatch = useDispatch();
+
     return(
         <nav className="navbar navbar-light" style={{'backgroundColor':'#e3f2fd', 'padding':'10px 20px'}}>
             <Link className="navbar-brand" to="/blog/home">Home</Link>
             <ul className="navbar-nav" style={{'flexDirection':'row'}}>
+                <li className="nav-item me-3 mt-1">
+                    <button className="btn btn-link text-decoration-none"
+                        onClick={() => {
+                            if(isLoggedIn){
+                                dispatch(logout())
+                            }else{
+                                dispatch(login())
+                            }
+                        }}>{isLoggedIn ? '로그인' : '로그아웃'}</button>
+                </li>
                 <li className="nav-item me-3">
                     <NavLink
                         exact
