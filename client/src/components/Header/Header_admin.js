adcc67faa6ca885cfb8b537a7d471d2dc0d94a23
DESKTOP-MBN1LUP\Z3A220803 <srwoo@hyundaiht.co.kr>
2023-02-06 08:49:28 +0900

    @ 삭제 2

--- client/src/components/Header/Header_admin.js
+++ client/src/components/Header/Header_admin.js
@@ -112,7 +112,7 @@ class Header extends Component {
                         </li>
                     </ul>
                     <div className="hd_right">
-                        <p><span>'{this.state.usernm}'</span>님 반갑습니다.@</p>
+                        <p><span>'{this.state.usernm}'</span>님 반갑습니다.</p>
                     </div>
                 </div>
             </div>
